8eb2223168b214bf08bbd404cd98bb9a35719385
lucimanluk <lucimanluk4000@gmail.com>
2024-05-19 18:51:33 +0300

    Added multi-threading.

--- src/museum/FrameSelector.java
+++ src/museum/FrameSelector.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package museum;
 
 import javax.swing.*;
@@ -11,8 +7,8 @@ import java.awt.event.ActionListener;
 
 public class FrameSelector extends JFrame {
 
-    private JButton MainFrame = new JButton("Main frame");
-    private JButton reservationPane = new JButton("Reservation");
+    private JButton mainFrameButton = new JButton("Main frame");
+    private JButton reservationPaneButton = new JButton("Reservation");
 
     public FrameSelector(String title, int admin) {
         
@@ -20,22 +16,44 @@ public class FrameSelector extends JFrame {
         
         this.setLayout(new GridLayout(2, 1));
 
-        MainFrame.addActionListener(new ActionListener() {
+        mainFrameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new MainFrame(admin);
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                new MainFrame(admin); 
+                            }
+                        });
+                    }
+                });
+                t.start();
             }
         });
         
-        reservationPane.addActionListener(new ActionListener() {
+        reservationPaneButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                new ReservationPane();
+            public void actionPerformed(ActionEvent e) {         
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {             
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                new ReservationPane();  
+                            }
+                        });
+                    }
+                });
+                t.start();
             }
         });
 
-        this.add(MainFrame);
-        this.add(reservationPane);
+        this.add(mainFrameButton);
+        this.add(reservationPaneButton);
 
         this.setVisible(true);
         this.setSize(400, 300);
